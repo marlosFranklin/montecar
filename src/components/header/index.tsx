6c5a506baa0f554c84ea7a9722eb9f93c0a34808
Marlos11 <marlos-gama@hotmail.com>
2026-07-27 15:56:26 -0300

    feat:estiliza  header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+import logoImg from "../../assets/logo.svg";
+import { FiUser, FiLogIn } from "react-icons/fi";
+const signed = false;
+const loadingAuth = false;
 export function Header() {
   return (
-    <div>
-      <h1>header</h1>
+    <div className="w-full flex items-center justify-center h-16 bg-white drop-shadow-2xl mb-4">
+      <header className="w-full max-w-7xl flex items-center justify-between px-4 mx-auto">
+        <Link to="/">
+          <img src={logoImg} alt="logo do site" />
+        </Link>
+        {!loadingAuth && signed && (
+          <Link to="/dashboard">
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiUser size={24} color="#000" />
+            </div>
+          </Link>
+        )}
+        {!loadingAuth && !signed && (
+          <Link to="/login">
+            <FiLogIn size={24} color="#000" />
+          </Link>
+        )}
+      </header>
     </div>
   );
 }

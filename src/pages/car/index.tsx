3cc2e5edd38444f842c9042951e03dcabbbe9119
Marlos11 <marlos-gama@hotmail.com>
2026-07-27 15:36:16 -0300

    feat: cria paginas, login, cadastro, dashboard, home, detalhes

---
+++ src/pages/car/index.tsx
@@ -0,0 +1,7 @@
+export function CarDetail() {
+  return (
+    <div>
+      <h1>detalhes do carro</h1>
+    </div>
+  );
+}
---
+++ src/pages/dashboard/new/index.tsx
@@ -0,0 +1,7 @@
+export function New() {
+  return (
+    <div>
+      <h1>cadastro carro</h1>
+    </div>
+  );
+}

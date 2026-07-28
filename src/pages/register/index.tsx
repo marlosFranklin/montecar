15a2e3249f90f773bdadf8b2e27329e6c83f38fb
Marlos11 <marlos-gama@hotmail.com>
2026-07-28 13:13:42 -0300

    feat:cria metodo para cadastro de usuario

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,7 +1,112 @@
+import { Link, useNavigate } from "react-router-dom";
+import logoImg from "../../assets/logo.svg";
+import { Container } from "../../components/container";
+import { Input } from "../../components/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { auth } from "../../services/firebaseConnection";
+import {
+  createUserWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { useEffect } from "react";
+
+const schema = z.object({
+  name: z.string().nonempty("O campo nome é obrigatório"),
+  email: z
+    .email("Insira um email válido")
+    .nonempty("O campo email é obrigatório"),
+  password: z
+    .string()
+    .min(6, "A senha deve conter pelo menos 6 caracteres")
+    .nonempty("O campo senha é obrigatório"),
+});
+
+type FormData = z.infer<typeof schema>;
+
 export function Register() {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  useEffect(() => {
+    async function handleLogout() {
+      await signOut(auth);
+    }
+    handleLogout();
+  }, []);
+
+  function onSubmit(data: FormData) {
+    createUserWithEmailAndPassword(auth, data.email, data.password)
+      .then(async (user) => {
+        await updateProfile(user.user, {
+          displayName: data.name,
+        });
+        console.log("CADASTRO COM SUCESSO");
+        navigate("/dashboard", { replace: true });
+      })
+      .catch((error) => {
+        console.log("error ao cadastrar este usuario");
+        console.log(error);
+      });
+  }
   return (
-    <div>
-      <h1>pagina cadastro</h1>
-    </div>
+    <Container>
+      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+        <Link to="/" className="mb-6 max-w-sm w-full">
+          <img src={logoImg} alt="logo do site" className="w-full" />
+        </Link>
+
+        <form
+          className="bg-white max-w-xl w-full rounded-xl p-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="mb-3">
+            <Input
+              type="text"
+              placeholder="Digite seu Nome completo"
+              name="name"
+              error={errors.name?.message}
+              register={register}
+            />
+          </div>
+          <div className="mb-3">
+            <Input
+              type="email"
+              placeholder="Digite seu email"
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+          <div className="mb-3">
+            <Input
+              type="password"
+              placeholder="Digite sua senha "
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+          <button
+            className="w-full bg-black text-white rounded-md h-10 px-2 font-bold"
+            type="submit"
+          >
+            Acessar
+          </button>
+        </form>
+        <Link to="/login">Já possui uma conta? Faça login</Link>
+      </div>
+    </Container>
   );
 }

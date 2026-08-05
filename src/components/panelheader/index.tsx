05b4d47904e5e49cb8aade952232413458d6f9b8
Marlos11 <marlos-gama@hotmail.com>
2026-08-05 16:19:40 -0300

    feat:cria novo componente de header para dashboard. adicionado navegacao para pagina de cadastro de novos veiculo e funcao para deslogar usuario

---
+++ src/components/panelheader/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../../services/firebaseConnection";
+
+export function DashboardHeader() {
+  const handleLogout = async () => {
+    await signOut(auth);
+  };
+  return (
+    <div className=" w-full flex items-center h-10 bg-red-500 rounded-lg text-white font-medium gap-4 px-4 mb-4">
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/dashboard/new">Cadastrar Novo Veículo</Link>
+
+      <button className="ml-auto" onClick={handleLogout}>
+        Sair da conta{" "}
+      </button>
+    </div>
+  );
+}

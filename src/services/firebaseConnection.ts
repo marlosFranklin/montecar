d3793404924e6572ac09a6568d1131192bf913d2
Marlos11 <marlos-gama@hotmail.com>
2026-08-11 14:16:15 -0300

    feat:add conexao com storage do firebase para upload de imagens

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -16,5 +17,6 @@ const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
-export { db, auth };
+export { db, auth, storage };

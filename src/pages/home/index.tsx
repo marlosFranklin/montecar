8393fa70e8b2db27c782ed740db05465e43ac9b8
Marlos11 <marlos-gama@hotmail.com>
2026-08-27 14:15:43 -0300

    feat: busca dados do veiculo no banco, para exibir na tela home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,52 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { collection, query, orderBy, getDocs } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
 import { Container } from "../../components/container";
 
 export function Home() {
+  interface CarsProps {
+    id: string;
+    uid: string;
+    name: string;
+    city: string;
+    year: string;
+    price: string | number;
+    km: string;
+    images: CarImageProps[];
+  }
+
+  interface CarImageProps {
+    id: string;
+    uid: string;
+    url: string;
+  }
+  const [cars, setCars] = useState<CarsProps[]>([]);
+
+  useEffect(() => {
+    function loadCars() {
+      const carsRef = collection(db, "cars");
+      const queryRef = query(carsRef, orderBy("created", "desc"));
+
+      getDocs(queryRef).then((snapshot) => {
+        let listCars = [] as CarsProps[];
+        snapshot.forEach((doc) => {
+          listCars.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            price: doc.data().price,
+            uid: doc.data().uid,
+            images: doc.data().images,
+          });
+        });
+        setCars(listCars);
+      });
+    }
+    loadCars();
+  }, []);
   return (
     <Container>
       <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
@@ -18,66 +64,32 @@ export function Home() {
         Carros novos e usados em todo Brasil
       </h1>
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full bg-white rounded-lg">
-          <img
-            className=" w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2026/202607/20260727/jeep-compass-1.3-t270-turbo-flex-longitude-at6-wmimagem0407032959.jpg"
-            alt="carro"
-          />
-          <p className="font-bold mt-1 mb-2 px-2">jeep</p>
-          <div className="flex flex-col px-2 ">
-            <span className="text-zinc-700 mb-6">Ano 2026/2026 | 23.00</span>
-            <strong className=" text-black font-medium text-xl">
-              {" "}
-              R$ 190.00
-            </strong>
-          </div>
-          <div className="w-full h-px bg-slate-200 my-2"></div>
+        {cars.map((car) => (
+          <Link key={car.id} to={`/car/${car.id}`}>
+            <section className="w-full bg-white rounded-lg">
+              <img
+                className=" w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+                src={car.images[0].url}
+                alt="carro"
+              />
+              <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
+              <div className="flex flex-col px-2 ">
+                <span className="text-zinc-700 mb-6">
+                  Ano {car.year} | {car.km} km
+                </span>
+                <strong className=" text-black font-medium text-xl">
+                  {" "}
+                  R$ {car.price}
+                </strong>
+              </div>
+              <div className="w-full h-px bg-slate-200 my-2"></div>
 
-          <div className="px-2 pb-2">
-            <span className="text-zinc-700">São Paulo - SP</span>
-          </div>
-        </section>
-        <section className="w-full bg-white rounded-lg">
-          <img
-            className=" w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2026/202607/20260727/jeep-compass-1.3-t270-turbo-flex-longitude-at6-wmimagem0407032959.jpg"
-            alt="carro"
-          />
-          <p className="font-bold mt-1 mb-2 px-2">jeep</p>
-          <div className="flex flex-col px-2 ">
-            <span className="text-zinc-700 mb-6">Ano 2026/2026 | 23.00</span>
-            <strong className=" text-black font-medium text-xl">
-              {" "}
-              R$ 190.00
-            </strong>
-          </div>
-          <div className="w-full h-px bg-slate-200 my-2"></div>
-
-          <div className="px-2 pb-2">
-            <span className="text-zinc-700">São Paulo - SP</span>
-          </div>
-        </section>
-        <section className="w-full bg-white rounded-lg">
-          <img
-            className=" w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2026/202607/20260727/jeep-compass-1.3-t270-turbo-flex-longitude-at6-wmimagem0407032959.jpg"
-            alt="carro"
-          />
-          <p className="font-bold mt-1 mb-2 px-2">jeep</p>
-          <div className="flex flex-col px-2 ">
-            <span className="text-zinc-700 mb-6">Ano 2026/2026 | 23.00</span>
-            <strong className=" text-black font-medium text-xl">
-              {" "}
-              R$ 190.00
-            </strong>
-          </div>
-          <div className="w-full h-px bg-slate-200 my-2"></div>
-
-          <div className="px-2 pb-2">
-            <span className="text-zinc-700">São Paulo - SP</span>
-          </div>
-        </section>
+              <div className="px-2 pb-2">
+                <span className="text-zinc-700">{car.city}</span>
+              </div>
+            </section>
+          </Link>
+        ))}
       </main>
     </Container>
   );

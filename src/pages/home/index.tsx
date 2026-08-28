9aa6676d89839cdef4c10f016af9e30a031d454d
Marlos11 <marlos-gama@hotmail.com>
2026-08-28 11:50:38 -0300

    feat: cria um estado para controlar quando a imagem é apresentada na tela junto a isso evitando layoutshift.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -22,6 +22,7 @@ export function Home() {
     url: string;
   }
   const [cars, setCars] = useState<CarsProps[]>([]);
+  const [loadImages, setLoadImages] = useState<string[]>([]);
 
   useEffect(() => {
     function loadCars() {
@@ -47,6 +48,10 @@ export function Home() {
     }
     loadCars();
   }, []);
+
+  function handleImageLoad(id: string) {
+    setLoadImages((prevImageLoaded) => [...prevImageLoaded, id]);
+  }
   return (
     <Container>
       <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
@@ -67,10 +72,20 @@ export function Home() {
         {cars.map((car) => (
           <Link key={car.id} to={`/car/${car.id}`}>
             <section className="w-full bg-white rounded-lg">
+              <div
+                className="w-full h-72 rounded-lg bg-slate-200"
+                style={{
+                  display: loadImages.includes(car.id) ? "none" : " block",
+                }}
+              ></div>
               <img
                 className=" w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
                 src={car.images[0].url}
                 alt="carro"
+                onLoad={() => handleImageLoad(car.id)}
+                style={{
+                  display: loadImages.includes(car.id) ? "block" : " none",
+                }}
               />
               <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
               <div className="flex flex-col px-2 ">

144b9f835748e53e712019ce9368979b4706e50c
Marlos11 <marlos-gama@hotmail.com>
2026-08-31 08:38:29 -0300

    feat:cria funcao para deletar veiculos. add dados dinamicos dos veiculos

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,7 +3,14 @@ import { DashboardHeader } from "../../components/panelheader";
 import { FiTrash2 } from "react-icons/fi";
 import { useState, useEffect, useContext } from "react";
 import { db } from "../../services/firebaseConnection";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 import { AuthContext } from "../../contexts/AuthContext";
 
 interface CarsProps {
@@ -50,36 +57,50 @@ export function Dashboard() {
           });
         });
         setCars(listCars);
-        console.log(listCars);
       });
     }
     loadCars();
   }, [user]);
+
+  async function handleDeleteCar(id: string) {
+    const docRef = doc(db, "cars", id);
+    await deleteDoc(docRef);
+    setCars(cars.filter((car) => car.id !== id));
+  }
   return (
     <Container>
       <DashboardHeader />
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full bg-white rounded-lg relative">
-          <button className=" absolute bg-white w-14 h-14 rounded-full flex items-center justify-center  top-2 right-2 drop-shadow-2xl ">
-            <FiTrash2 size={26} color="#000" />
-          </button>
-          <img
-            src="https://firebasestorage.googleapis.com/v0/b/webcarros-c24cb.firebasestorage.app/o/images%2FQ7gUpXFPgCejh0QnvaFrLCFCAI73%2Fcc7b2ac8-2fed-46b4-8667-8e5b790aa19f?alt=media&token=5da4b40f-e23f-4317-9ca9-d40e26e42d13"
-            alt=""
-            className="w-full rounded-lg mb-2 max-h-70"
-          />
-          <p className="font-bold mt-1 mb-2 px-2">Mercedez</p>
+        {cars.map((car) => (
+          <section key={car.id} className="w-full bg-white rounded-lg relative">
+            <button
+              onClick={() => handleDeleteCar(car.id)}
+              className=" absolute bg-white w-14 h-14 rounded-full flex items-center justify-center  top-2 right-2 drop-shadow-2xl "
+            >
+              <FiTrash2 size={26} color="#000" />
+            </button>
+            <img
+              src={car.images[0].url}
+              alt={`foto veiculo ${car.name}`}
+              className="w-full rounded-lg mb-2 max-h-70"
+            />
+            <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
 
-          <div className="flex flex-col px-2">
-            <span>Ano:2016/216 | 230.00</span>
-            <strong className="text-black font-bold mt-4 ">R$:150.00</strong>
-          </div>
+            <div className="flex flex-col px-2">
+              <span>
+                Ano:{car.year} | {car.km} km
+              </span>
+              <strong className="text-black font-bold mt-4 ">
+                R$:{car.price}
+              </strong>
+            </div>
 
-          <div className="w-full h-px bg-slate-200 my-2"></div>
-          <div className=" px-2 pb-2">
-            <span className="text-black">Guarulhos-SP</span>
-          </div>
-        </section>
+            <div className="w-full h-px bg-slate-200 my-2"></div>
+            <div className=" px-2 pb-2">
+              <span className="text-black">{car.city}</span>
+            </div>
+          </section>
+        ))}
       </main>
     </Container>
   );
